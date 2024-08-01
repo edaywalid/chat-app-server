2be4ef54fa8d59a7dd6ecf238ca1434982ce5dab
edaywalid <becharwalid9@gmail.com>
2024-08-01 15:09:53 +0100

    set up user repository

---
+++ internal/handlers/auth_handler.go
@@ -0,0 +1,60 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/edaywalid/chat-app/internal/services"
+	"github.com/gin-gonic/gin"
+)
+
+type AuthHandler struct {
+	authService *services.AuthService
+}
+
+func NewAuthHandler(authService *services.AuthService) *AuthHandler {
+	return &AuthHandler{authService}
+}
+
+func (h *AuthHandler) Register(c *gin.Context) {
+	var input struct {
+		Username string `json:"username" binding:"required"`
+		Email    string `json:"email" binding:"required,email"`
+		Password string `json:"password" binding:"required,min=6"`
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	tokenPair, err := h.authService.Register(input.Username, input.Email, input.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to register user"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message":       "User registered successfully",
+		"access-token":  tokenPair.AccessToken,
+		"refresh-token": tokenPair.RefreshToken,
+	})
+}
+
+func (h *AuthHandler) Login(c *gin.Context) {
+	var input struct {
+		Username string `json:"username"`
+		Password string `json:"password" binding:"required"`
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := h.authService.Login(input.Username, input.Password); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User Logged In"})
+}
---
+++ internal/repositories/user_repository.go
@@ -0,0 +1,15 @@
+package repositories
+
+import (
+	"github.com/edaywalid/chat-app/internal/models"
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{db}
+}
+
