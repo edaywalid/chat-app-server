f08311a396851398ab04868132c4733e8df845e9
edaywalid <becharwalid9@gmail.com>
2024-08-01 15:10:20 +0100

    added FindByUsername function

--- internal/repositories/user_repository.go
+++ internal/repositories/user_repository.go
@@ -17,3 +17,8 @@ func (r *UserRepository) Create(user *models.User) error {
 	return r.db.Create(user).Error
 }
 
+func (r *UserRepository) FindByUsername(username string) (*models.User, error) {
+	var user models.User
+	err := r.db.Where("username = ?", username).First(&user).Error
+	return &user, err
+}
