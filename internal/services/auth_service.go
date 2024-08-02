3838dfd2e9079b4ef21cc8f3fec6d24500359dc8
edaywalid <becharwalid9@gmail.com>
2024-08-02 10:46:28 +0100

    set up email confirmation process

--- internal/services/auth_service.go
+++ internal/services/auth_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"errors"
+	"time"
 
 	"github.com/edaywalid/chat-app/internal/models"
 	"github.com/edaywalid/chat-app/internal/repositories"
@@ -9,46 +10,51 @@ import (
 )
 
 type AuthService struct {
-	userRepo   *repositories.UserRepository
-	jwtService *JwtService
+	userRepo     *repositories.UserRepository
+	jwtService   *JwtService
+	emailService *utils.EmailService
 }
 
 func NewAuthService(
 	userRepo *repositories.UserRepository,
 	jwtService *JwtService,
+	emailService *utils.EmailService,
 ) *AuthService {
 	return &AuthService{
 		userRepo,
 		jwtService,
+		emailService,
 	}
 }
 
-func (s *AuthService) Register(username, email, password string) (*TokenPair, error) {
+func (s *AuthService) Register(username, email, password string) error {
 	if !utils.ValidatePassword(password) {
-		return nil, errors.New("password not secure")
+		return errors.New("password not secure")
 	}
 	hashedPassword, err := utils.HashPassword(password)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	user := &models.User{
-		Username: username,
-		Email:    email,
-		Password: string(hashedPassword),
+		Username:                    username,
+		Email:                       email,
+		Password:                    string(hashedPassword),
+		EmailConfirmationCode:       utils.GenerateRandomCode(6),
+		EmailConfirmationCodeExpiry: time.Now().Add(time.Minute * 15),
 	}
 
 	err = s.userRepo.Create(user)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	tokenPair, err := s.jwtService.GenerateTokenPair(user.ID)
+	err = s.emailService.SendEmail("Welcome to chat app", "your confirmation code is : "+user.EmailConfirmationCode, user.Email)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return tokenPair, nil
+	return nil
 }
 
 func (s *AuthService) Login(username, password string) error {
@@ -62,5 +68,48 @@ func (s *AuthService) Login(username, password string) error {
 		return err
 	}
 
+	if !user.IsVerified {
+
+		user.EmailConfirmationCode = utils.GenerateRandomCode(6)
+		user.EmailConfirmationCodeExpiry = time.Now().Add(time.Minute * 15)
+
+		err = s.userRepo.Update(user)
+		if err != nil {
+			return err
+		}
+
+		err = s.emailService.SendEmail("Welcome to chat app", "your confirmation code is : "+user.EmailConfirmationCode, user.Email)
+		if err != nil {
+			return err
+		}
+
+		return errors.New("email not verified")
+	}
+
 	return nil
+}
+
+func (s *AuthService) ConfirmEmail(email, code string) error {
+	user, err := s.userRepo.FindByEmail(email)
+	if err != nil {
+		return err
+	}
+
+	if user.EmailConfirmationCode == code {
+		if user.EmailConfirmationCodeExpiry.Before(time.Now()) {
+			user.IsVerified = true
+			user.EmailConfirmationCode = ""
+			user.EmailConfirmationCodeExpiry = time.Time{}
+
+			err := s.userRepo.Update(user)
+			if err != nil {
+				return err
+			}
+			return nil
+		} else {
+			return errors.New("confirmation code Expired")
+		}
+	} else {
+		return errors.New("confirmation code incorrect")
+	}
 }
