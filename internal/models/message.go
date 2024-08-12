f30ad936c2548702a15e52a496b56ef1f26bd7c9
edaywalid <becharwalid9@gmail.com>
2024-08-12 10:44:58 +0100

    added message model

--- internal/models/message.go
+++ internal/models/message.go
@@ -1,1 +1,9 @@
 package models
+
+type Message struct {
+	ID          string `json:"id" bson:"_id"`
+	SenderID    string `json:"sender_id" bson:"sender_id"`
+	RecipientID string `json:"recipient_id" bson:"recipient_id"`
+	Content     string `json:"content" bson:"content"`
+	CreatedAt   string `json:"created_at" bson:"created_at"`
+}
