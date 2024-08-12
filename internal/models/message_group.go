1b3dd2a9107afdc9b0c41fbfabb9e4a18219daaa
edaywalid <becharwalid9@gmail.com>
2024-08-12 10:45:17 +0100

    added message_group model

--- internal/models/message_group.go
+++ internal/models/message_group.go
@@ -1,1 +1,9 @@
 package models
+
+type GroupMessage struct {
+	ID        string `json:"id" bson:"_id"`
+	SenderID  string `json:"sender_id" bson:"sender_id"`
+	GroupID   string `json:"group_id" bson:"group_id"`
+	Content   string `json:"content" bson:"content"`
+	CreatedAt string `json:"created_at" bson:"created_at"`
+}
