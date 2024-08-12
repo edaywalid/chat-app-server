d4eb3b6b1e73a86a77a6169ad26e730e735ce739
edaywalid <becharwalid9@gmail.com>
2024-08-12 10:46:00 +0100

    updated jwt service to generate new token from refresh token

--- internal/services/jwt_service.go
+++ internal/services/jwt_service.go
@@ -21,48 +21,77 @@ type TokenPair struct {
 	RefreshToken string
 }
 
+func (s *JwtService) generateToken(userID uuid.UUID, live_time time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(live_time).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(s.config.JWTSecret))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
 func (s *JwtService) GenerateTokenPair(userID uuid.UUID) (*TokenPair, error) {
-	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"user_id": userID,
-		"exp":     time.Now().Add(time.Minute * 15).Unix(),
-	})
-
-	accessTokenString, err := accessToken.SignedString([]byte(s.config.JWTSecret))
+	accessToken, err := s.generateToken(userID, time.Minute*15)
 	if err != nil {
 		return nil, err
 	}
 
-	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"user_id": userID,
-		"exp":     time.Now().Add(time.Hour * 24 * 7).Unix(),
-	})
-
-	refreshTokenString, err := refreshToken.SignedString([]byte(s.config.JWTSecret))
+	refreshToken, err := s.generateToken(userID, time.Hour*24*7)
 	if err != nil {
 		return nil, err
 	}
-
 	return &TokenPair{
-		AccessToken:  accessTokenString,
-		RefreshToken: refreshTokenString,
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
 	}, nil
 }
 
 func (s *JwtService) ValidateToken(tokenString string) (uuid.UUID, error) {
-	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		return []byte(s.config.JWTSecret), nil
 	})
 	if err != nil {
-		return uuid.UUID{}, err
+		return uuid.Nil, err
 	}
 
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		userID, err := uuid.Parse(claims["user_id"].(string))
-		if err != nil {
-			return uuid.UUID{}, err
+	if !token.Valid {
+		return uuid.Nil, jwt.ValidationError{
+			Errors: jwt.ValidationErrorExpired,
 		}
-		return userID, nil
 	}
 
-	return uuid.UUID{}, jwt.ErrSignatureInvalid
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return uuid.Nil, jwt.ValidationError{
+			Errors: jwt.ValidationErrorClaimsInvalid,
+		}
+	}
+
+	userID, ok := claims["user_id"].(string)
+	if !ok {
+		return uuid.Nil, jwt.ValidationError{
+			Errors: jwt.ValidationErrorClaimsInvalid,
+		}
+	}
+
+	return uuid.Parse(userID)
+}
+
+func (s *JwtService) RefreshToken(refreshToken string) (string, error) {
+	userID, err := s.ValidateToken(refreshToken)
+	if err != nil {
+		return "", err
+	}
+
+	token, err := s.generateToken(userID, time.Minute*15)
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
 }
