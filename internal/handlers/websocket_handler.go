78a4a90d4919db047dd72ab4ecc11a7962a73b83
edaywalid <becharwalid9@gmail.com>
2024-09-11 04:24:20 +0100

    better error handling

--- internal/handlers/websocket_handler.go
+++ internal/handlers/websocket_handler.go
@@ -2,7 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 	"time"
 
@@ -12,6 +12,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 var upgrader = websocket.Upgrader{
@@ -35,36 +36,51 @@ func NewWebSocketHandler(wsManager *internal_ws.Manager, chatService *services.C
 func (h *WebSocketHandler) SendOneOnOneMessage(c *gin.Context) {
 	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
+		log.Printf("error upgrading to websocket: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error upgrading to websocket"})
 		return
 	}
 
 	extracted_userid, exists := c.Get("user_id")
 
 	if !exists {
+		log.Printf("error getting user_id from context: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error getting user_id from context"})
 		return
 	}
 	userID := extracted_userid.(uuid.UUID)
 	h.wsManager.AddClient(userID, conn)
-	defer h.wsManager.RemoveClient(userID)
+
+	defer func() {
+		h.wsManager.RemoveClient(userID)
+		conn.Close()
+	}()
+
 	go h.chatService.ListenForDirectMessage(userID)
 	for {
 		_, message, err := conn.ReadMessage()
 		if err != nil {
-			panic(err)
-			return
+			log.Printf("error reading message: %v", err)
+			break
 		}
 		messageModel := &models.Message{}
 		err = json.Unmarshal(message, &messageModel)
 		if err != nil {
-			panic(err)
-			return
+			log.Printf("error unmarshalling message: %v", err)
+			conn.WriteMessage(websocket.TextMessage, []byte("error unmarshalling message"))
+			continue
 		}
 
+		messageModel.ID = primitive.NewObjectID()
 		messageModel.SenderID = userID.String()
 		messageModel.CreatedAt = time.Now()
 
 		if messageModel.RecipientID != "" {
 			err = h.chatService.SendDirectMessage(messageModel)
+			if err != nil {
+				log.Printf("error sending direct message: %v", err)
+				conn.WriteMessage(websocket.TextMessage, []byte("error sending direct message"))
+			}
 		}
 	}
 }
@@ -72,35 +88,47 @@ func (h *WebSocketHandler) SendOneOnOneMessage(c *gin.Context) {
 func (h *WebSocketHandler) SendGroupMessage(c *gin.Context) {
 	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
-		panic(err)
+		log.Printf("error upgrading to websocket: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error upgrading to websocket"})
 		return
 	}
 
 	extracted_userid, exists := c.Get("user_id")
 	if !exists {
-		panic(err)
+		log.Printf("error getting user_id from context: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error getting user_id from context"})
 		return
 	}
 	userID := extracted_userid.(uuid.UUID)
 	h.wsManager.AddClient(userID, conn)
-	defer h.wsManager.RemoveClient(userID)
+	defer func() {
+		h.wsManager.RemoveClient(userID)
+		conn.Close()
+	}()
 
 	for {
 		_, message, err := conn.ReadMessage()
 		if err != nil {
-			panic(err)
+			log.Printf("error reading message: %v", err)
 			break
 		}
 		messageModel := &models.GroupMessage{}
 		err = json.Unmarshal(message, &messageModel)
 		if err != nil {
-			panic(err)
-			break
+			log.Printf("error unmarshalling message: %v", err)
+			conn.WriteMessage(websocket.TextMessage, []byte("error unmarshalling message"))
+			continue
 		}
+
+		messageModel.ID = primitive.NewObjectID()
 		messageModel.SenderID = userID.String()
 
 		if messageModel.GroupID == "" {
 			err = h.chatService.SendGroupMessage(messageModel)
+			if err != nil {
+				log.Printf("error sending group message: %v", err)
+				conn.WriteMessage(websocket.TextMessage, []byte("error sending group message"))
+			}
 		}
 	}
 }
@@ -108,32 +136,46 @@ func (h *WebSocketHandler) SendGroupMessage(c *gin.Context) {
 func (h *WebSocketHandler) SendBroadcastMessage(c *gin.Context) {
 	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
-		panic(err)
+		log.Printf("error upgrading to websocket: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error upgrading to websocket"})
 		return
 	}
 
 	extracted_userid, exists := c.Get("user_id")
 	if !exists {
-		panic(err)
+		log.Printf("error getting user_id from context: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error getting user_id from context"})
 		return
 	}
 	userID := extracted_userid.(uuid.UUID)
 	h.wsManager.AddClient(userID, conn)
-	defer h.wsManager.RemoveClient(userID)
+
+	defer func() {
+		h.wsManager.RemoveClient(userID)
+		conn.Close()
+	}()
 
 	for {
 		_, message, err := conn.ReadMessage()
 		if err != nil {
-			panic(err)
+			log.Printf("error reading message: %v", err)
 			break
 		}
 		messageModel := &models.Message{}
 		err = json.Unmarshal(message, &messageModel)
 		if err != nil {
-			panic(err)
-			break
+			log.Printf("error unmarshalling message: %v", err)
+			conn.WriteMessage(websocket.TextMessage, []byte("error unmarshalling message"))
+			continue
 		}
+
+		messageModel.ID = primitive.NewObjectID()
 		messageModel.SenderID = userID.String()
+
 		err = h.chatService.BroadcastMessage(messageModel)
+		if err != nil {
+			log.Printf("error sending broadcast message: %v", err)
+			conn.WriteMessage(websocket.TextMessage, []byte("error sending broadcast message"))
+		}
 	}
 }
